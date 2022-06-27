36412bfcd22e147bdcc0bd34d96a1bec026efcbc
mdmahfujarrahmane <ahmedmahfuj28@gmail.comm>
2022-06-27 21:05:18 +0600

    redux practice on react

---
+++ react-redux/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React-REDUX</h1>
+      <Counter/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-redux/src/components/Counter.js
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrementCounter, incrementCounter, resetCounter } from "../services/actions/counterAction";
+
+const Counter = () => {
+    const count = useSelector(state => state.count)
+    const dispatch = useDispatch()
+
+    const handleIncrement = () => {
+        dispatch(incrementCounter())
+    }
+    const handleDecrement = () => {
+        dispatch(decrementCounter())
+    }
+    const handleReset = () => {
+        dispatch(resetCounter())
+    }
+ 
+    return (
+        <div>
+            <h2>Counter App</h2>
+            <h3>Count: {count}</h3>
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Increment</button>
+            <button onClick={handleReset}>Increment</button>
+        </div>
+    );
+};
+
+export default Counter;
+
+
